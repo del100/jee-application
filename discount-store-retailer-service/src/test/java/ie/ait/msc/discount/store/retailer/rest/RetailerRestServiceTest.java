f907ed31f5863e088e6f7ef0f57a6463835dafa6
eeiwdey <wayne.delaney@ericsson.com>
2015-04-22 22:45:28 +0100

    added new Junit test

--- discount-store-retailer-service/src/test/java/ie/ait/msc/discount/store/retailer/rest/RetailerRestServiceTest.java
+++ discount-store-retailer-service/src/test/java/ie/ait/msc/discount/store/retailer/rest/RetailerRestServiceTest.java
@@ -3,6 +3,7 @@ package ie.ait.msc.discount.store.retailer.rest;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import ie.ait.msc.discount.store.dto.OfferDto;
+import ie.ait.msc.discount.store.dto.RetailerDto;
 import ie.ait.msc.discount.store.retailer.ejb.RetailerServiceLocal;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -25,24 +27,43 @@ public class RetailerRestServiceTest {
     private RetailerRestServiceImpl retailerRestService;
 
     @Mock
-    RetailerServiceLocal retailerService;
+    private RetailerServiceLocal mockRetailerService;
+    @Mock
+    private OfferDto mockOffer;
+    @Mock
+    private RetailerDto mockRetailer;
+
+    private List<OfferDto> offers = new ArrayList<OfferDto>();
 
     @Before
     public void setup() {
         retailerRestService = new RetailerRestServiceImpl();
+        offers.add(mockOffer);
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void getAllOffers_success() {
-        List<OfferDto> offers = new ArrayList<OfferDto>();
-        OfferDto offer = new OfferDto();
-        offers.add(offer);
-        when(retailerService.getAllOffers("user")).thenReturn(offers);
+        // setup
+        when(mockRetailerService.getAllOffers("user")).thenReturn(offers);
 
+        // execute
         Response response = retailerRestService.getAllOffers("user");
 
+        // verify
         assertTrue(response.getStatus() == 200);
         assertTrue(response.getEntity().equals(offers));
+    }
+
+    @Test
+    public void addCreateAccount_success() {
+        // setup
+        Mockito.doNothing().when(mockRetailerService).createAccount(mockRetailer);
+
+        // execute
+        Response response = retailerRestService.createAccount(mockRetailer);
+
+        // verify
+        assertTrue(response.getStatus() == 200);
     }
 }
